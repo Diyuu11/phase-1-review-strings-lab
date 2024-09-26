31f010e64fd3394c627340f939ec3f4a6655a745
hamdiabdiaden <diagodu198@gmail.com>
2024-09-26 11:54:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'hamdi';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = 'Welcome to Flatbook, hamdi!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = 'Welcome, hamdi!';
+const shortGreeting = `Welcome, h!`;
